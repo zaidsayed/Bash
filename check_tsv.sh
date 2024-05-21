d0431004bacb708ec12a8fd873d36bf4650b0aa7
Zaid Sayed <52396092+zaidsayed@users.noreply.github.com>
2024-05-21 18:35:30 +0800

    Remove the column with header Continent, which is sparsely populated and is not present in one of the files.

--- check_tsv.sh
+++ check_tsv.sh
@@ -1,35 +1,57 @@
-#!/bin/bash
+!/bin/bash
 
-# Function to check if a file is a TSV file based on the header
+# Function to check if a file is a TSV file based on the header and check for line discrepancies
 check_tsv() {
     local file="$1"
+    local output_file="${file%.tsv}_processed.tsv"
     local header=$(head -n 1 "$file")
-    
+
     if [[ "$header" == *$'\t'* ]]; then
         echo "$file is a TSV file."
     else
         echo "$file is not a TSV file."
     fi
 
+    # Identify the column number for the "Continent" header
+    local continent_col=$(echo "$header" | awk -F'\t' '{for (i=1; i<=NF; i++) if ($i == "Continent") print i}')
+
     # Count the number of columns in the header
     local num_columns=$(echo "$header" | awk -F'\t' '{print NF}')
-    
-    # Initialize line number
-    local line_number=1
 
-    # Read through the file line by line
-    while IFS= read -r line; do
-        # Increment line number
-        ((line_number++))
-
-        # Count the number of columns in the current line
-        local num_columns_line=$(echo "$line" | awk -F'\t' '{print NF}')
-
-        # Check if the number of columns matches the header
-        if [[ "$num_columns_line" -ne "$num_columns" ]]; then
-            echo "Line $line_number in $file does not have the same number of cells as the header." >&2
-        fi
-    done < "$file"
+    # Use awk to process the file
+    awk -v num_columns="$num_columns" -v file="$file" -v continent_col="$continent_col" '
+    BEGIN { FS="\t"; OFS="\t"; }
+    NR == 1 {
+        if (continent_col) {
+            for (i=1; i<=NF; i++) {
+                if (i != continent_col) {
+                    printf "%s%s", $i, (i < NF ? OFS : ORS)
+                }
+            }
+            print ""
+        } else {
+            print
+        }
+    }
+    NR > 1 {
+        if (continent_col) {
+            if (NF != num_columns) {
+                print "Line " NR " in " file " does not have the same number of cells as the header." > "/dev/stderr";
+            }
+            for (i=1; i<=NF; i++) {
+                if (i != continent_col) {
+                    printf "%s%s", $i, (i < NF ? OFS : ORS)
+                }
+            }
+            print ""
+        } else {
+            if (NF != num_columns) {
+                print "Line " NR " in " file " does not have the same number of cells as the header." > "/dev/stderr";
+            } else {
+                print
+            }
+        }
+    }' "$file" > "$output_file"
 }
 
 # Iterate over all input files
@@ -37,6 +59,5 @@ for file in "$@"; do
     if [[ -f "$file" ]]; then
         check_tsv "$file"
     else
-        echo "$file does not exist." >&2
-    fi
-done
+        echo "$file does not exist."
+
